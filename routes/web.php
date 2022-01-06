6fcc50d699bc6dd2efebbb2f05b0a34cdacc5825
hide <hide.micchi.shizuoka@gmail.com>
2022-01-06 23:22:53 +0900

    課題14

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,22 @@ Route::group(['prefix' => 'admin'], function(){
   Route::get('profile/edit', 'Admin\ProfileController@edit')->middleware('auth');
 });
 
+//以下14章追記
+Route::group(['prefix' => 'admin', 'middleware'=> 'auth'], function(){
+  Route::get('news/create', 'Admin\NewsController@add');
+  Route::post('news/create','Admin\NewsController@create');
+});
 //prefix->は、無名関数functionの中のURLをadminにしている
+//上のグループに入れるときは->middleware authを末尾につけないとダメっぽい
+//Profileはprofileでグループを分けたほうがわかりやすいかもしれない。
+//Routepostはここでは、create.blade.phpにformactionとmethod postがある。getと違ってフォームアクションでポストと書かないとダメっぽい
+//データが送信されると、（送信ボタンなどで）ルートが発動して、コントローラーのクリエエイトアクションに飛ばされる。
+//これは通常のページの表示にはGETを受け取り、フォームを送信したときに受け取る場合にはPOSTを受け取るように指定しています。
+
+Route::group(['prefix' => 'admin', 'middleware'=> 'auth'], function(){
+  Route::post('profile/create','Admin\ProfileController@create');//課題１４－３
+  Route::post('profile/edit', 'Admin\ProfileController@update');//課題１４－６
+});
 
 Auth::routes();
 
